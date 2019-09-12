25c09082c727804bd7ebe93ff42c58d509b6593b
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-09-12 10:31:02 -0500

    Volt Basic ability in game
    Works

--- AlmostRace-Capstone/Assets/Scripts/Ability.cs
+++ AlmostRace-Capstone/Assets/Scripts/Ability.cs
@@ -7,3 +7,11 @@ public abstract class Ability : MonoBehaviour
     public abstract void ActivateAbility();
     public abstract void DeactivateAbility();
 }
+
+public abstract class BasicAbility : Ability
+{
+    public CarHeatManager carHeatInfo;
+    [Tooltip("How much damage is done to the player every X seconds")] public float selfHeatDamage;
+    protected abstract void AddHeat();
+
+}
