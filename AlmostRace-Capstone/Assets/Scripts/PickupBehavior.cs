fb6aa83fffbd9808d197316e762671d15f324ffc
ldcaballero <37883634+ldcaballero@users.noreply.github.com>
2019-09-12 11:27:36 -0500

    Pickup Script Started

---
+++ AlmostRace-Capstone/Assets/Scripts/PickupBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupBehavior : MonoBehaviour
+{
+    public enum PickUps{misslePickUp };
+    public PickUps chosenPickUp;
+    private Ability pickUp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+
+        if (collision.gameObject.CompareTag("Vehicle"))
+        {
+            if(collision.gameObject.GetComponent<VehicleAbilityBehavior>().hasPickup() == false)
+            {
+                if (chosenPickUp == PickUps.misslePickUp)
+                {
+                    pickUp = Resources.Load<Ability>("MissileAbility");
+                    collision.gameObject.GetComponent<VehicleAbilityBehavior>().assignPickup(pickUp);
+                }
+                
+            }
+        }
+    }
+}
