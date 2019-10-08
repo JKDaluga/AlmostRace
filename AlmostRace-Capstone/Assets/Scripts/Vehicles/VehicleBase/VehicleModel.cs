1c590dcaa47024df84680bdcf21323f9d1538d10
JKDaluga <31351049+JKDaluga@users.noreply.github.com>
2019-10-08 11:26:15 -0500

    detached model from car scripts
    Galaxy Brain Manuveurs.

---
+++ AlmostRace-Capstone/Assets/Scripts/Vehicles/VehicleBase/VehicleModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleModel : MonoBehaviour
+{
+
+    public Transform sphere;
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = sphere.position;
+    }
+}
