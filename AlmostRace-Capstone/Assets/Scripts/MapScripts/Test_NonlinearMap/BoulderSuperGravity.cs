682908a193b65cee1473671e5bad0e5fcc85f6f2
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-10-08 11:41:48 -0500

    butts

---
+++ AlmostRace-Capstone/Assets/Scripts/MapScripts/Test_NonlinearMap/BoulderSuperGravity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderSuperGravity : MonoBehaviour
+{
+    public float downForce = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody>().AddForce(Vector3.down * downForce, ForceMode.Impulse);
+    }
+    private void FixedUpdate()
+    {
+        gameObject.GetComponent<Rigidbody>().AddForce(Vector3.down * downForce, ForceMode.Impulse);
+    }
+}
