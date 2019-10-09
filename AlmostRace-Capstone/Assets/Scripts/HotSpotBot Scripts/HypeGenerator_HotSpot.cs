b06ce8c5313cc582ddf991c83a6038187d6b1dd4
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-10-08 21:08:55 -0500

    collision confusion

---
+++ AlmostRace-Capstone/Assets/Scripts/HotSpotBot Scripts/HypeGenerator_HotSpot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HypeGenerator_HotSpot : MonoBehaviour
+{
+    private List<VehicleHypeBehavior> _vehiclesHyped;
+    public float hypeGained = 1;
+    public float hypeRate = 0.5f;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<VehicleHypeBehavior>() != null)
+        {
+            _vehiclesHyped.Add(other.gameObject.GetComponent<VehicleHypeBehavior>());
+        }
+    }
+}
