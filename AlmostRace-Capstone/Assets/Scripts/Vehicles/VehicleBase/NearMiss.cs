116fe5606a014820a83123aeb1b65b1dc75aca64
Robyn Riley <rriley818@gmail.com>
2019-10-24 13:01:03 -0500

    Rough implementation of Near Miss system
    Implemented in my Multiplayer Testing Scene, not pushed to prefab until designers can test and tweak it.

---
+++ AlmostRace-Capstone/Assets/Scripts/Vehicles/VehicleBase/NearMiss.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NearMiss : MonoBehaviour
+{
+    public List<GameObject> nearHits = new List<GameObject>();
+    public List<GameObject> hits = new List<GameObject>();
+    public LayerMask missable;
+    public VehicleHypeBehavior hype;
+
+    private void FixedUpdate()
+    {
+
+        RaycastHit[] closecall;
+
+        closecall = Physics.SphereCastAll(transform.position, 5, transform.position, 10, missable);
+
+        Debug.Log(closecall.Length);
+
+        for (int i = 0; i < closecall.Length; i++)
+        {
+            if(!nearHits.Find(GameObject => GameObject == closecall[i].collider.gameObject) && !hits.Find(GameObject => GameObject == closecall[i].collider.gameObject))
+            {
+                nearHits.Add(closecall[i].collider.gameObject);
+            }
+        }
+
+        foreach (GameObject target in nearHits)
+        {
+            if (Vector3.Distance(transform.position, target.transform.position) > 12)
+            {
+                nearHits.Remove(target);
+                if (!hits.Find(GameObject => GameObject == target))
+                {
+                    hype.AddHype(5.0f);
+                }
+                else
+                {
+                    hits.Remove(target);
+                }
+            }
+        }
+
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(nearHits.Find(GameObject => GameObject == collision.gameObject))
+        {
+            hits.Add(collision.gameObject);
+        }
+    }
+}
