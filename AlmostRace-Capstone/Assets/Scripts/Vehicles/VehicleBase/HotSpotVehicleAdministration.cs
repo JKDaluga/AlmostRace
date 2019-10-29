3e2f29acc2a9e7c534074112bcd4936f2d62fe15
Jake Velicer <007velicer@gmail.com>
2019-10-29 01:24:08 -0500

    Hot Spot Bot Hold and Drop backend

---
+++ AlmostRace-Capstone/Assets/Scripts/Vehicles/VehicleBase/HotSpotVehicleAdministration.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotSpotVehicleAdministration : MonoBehaviour
+{
+    public bool holdingTheBot;
+    private GameObject HotSpotBotHeld;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // For testing
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            DropTheBot();
+        }
+    }
+
+    private void HoldTheBot(GameObject theBot)
+    {
+        holdingTheBot = true;
+        HotSpotBotHeld = theBot.transform.parent.gameObject;
+        HotSpotBotHeld.GetComponent<HotSpotBotBehavior>().SetBeingHeld(true);
+        HotSpotBotHeld.SetActive(false);
+    }
+
+    public void DropTheBot()
+    {
+        holdingTheBot = false;
+        HotSpotBotHeld.SetActive(true);
+        HotSpotBotHeld.GetComponent<HotSpotBotBehavior>().SetBeingHeld(false);
+        HotSpotBotHeld.GetComponent<HotSpotBotBehavior>().SetPosition(transform.position);
+        HotSpotBotHeld = null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("HotSpot"))
+        {
+            if(!other.gameObject.transform.parent.GetComponent<HotSpotBotBehavior>().GetBeingHeld()
+            && !holdingTheBot && HotSpotBotHeld == null)
+            {
+                HoldTheBot(other.gameObject);
+            }
+        }
+    }
+}
