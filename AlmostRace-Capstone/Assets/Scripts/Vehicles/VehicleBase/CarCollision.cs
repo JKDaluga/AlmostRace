892e92cd1c51909068e22e477915d7f2f0ca4d64
JKDaluga <31351049+JKDaluga@users.noreply.github.com>
2019-11-09 16:44:39 -0600

    Car on Car Collisions Calculation Tweak

--- AlmostRace-Capstone/Assets/Scripts/Vehicles/VehicleBase/CarCollision.cs
+++ AlmostRace-Capstone/Assets/Scripts/Vehicles/VehicleBase/CarCollision.cs
@@ -30,22 +30,36 @@ public class CarCollision : MonoBehaviour
         CarCollision other = collision.gameObject.GetComponent<CarCollision>();
         if (other != null && _invulTime <= 0f)
         {
-            _invulTime = .2f;
+            _invulTime = .5f;
             Rigidbody rb = GetComponent<Rigidbody>();
             Rigidbody other_rb = other.GetComponent<Rigidbody>();
             SphereCarController otherCar = other.car.GetComponent<SphereCarController>();
             CarHeatManager otherCarHeat = other.car.GetComponent<CarHeatManager>();
-            Vector3 relativeVelocity = other_rb.velocity - rb.velocity;
-            Vector3 relativePosition = other_rb.position - rb.position;
-            float f = Vector3.Angle(relativePosition, rb.velocity);
-            if (f < 110)
+            Vector3 relativeVelocity = rb.velocity - other_rb.velocity;
+            Vector3 relativePosition = rb.position - other_rb.position;
+
+            //calculating the angle in radians then converting it to degrees
+            float angleBetween = Mathf.Acos(Vector3.Dot(relativeVelocity.normalized, relativePosition.normalized))* 180 / Mathf.PI;
+            
+            // We only want the player instigating the collision to damage the other so this angle helps determing who the instigating player is
+            float instigatingPlayerAngle = Mathf.Acos(Vector3.Dot(rb.velocity.normalized, -relativePosition.normalized)) * 180 / Mathf.PI;
+
+            // If the angleBetween < 90 then the collision exists
+            // If instigatingPlayerAngle < 90 then that player is moving in the direction of the other player and should assign damage
+            if (angleBetween < 90 & instigatingPlayerAngle < 90)
             {
-                float percentDamage = Vector3.Project(relativePosition.normalized, rb.velocity.normalized).magnitude;
-                float damage = percentDamage * weight * relativeVelocity.magnitude;
-                otherCarHeat.heatCurrent += damage;
-                car.GetComponent<VehicleHypeBehavior>().AddHype(hypeToAdd);
+                float percentDamage = 0f;
+                if (rb.velocity.magnitude > 10f)
+                {
+                    percentDamage = Vector3.Project(relativePosition.normalized, rb.velocity.normalized).magnitude;
+                }
+                if (percentDamage > 0)
+                {
+                    float damage = percentDamage * weight * relativeVelocity.magnitude;
+                    otherCarHeat.heatCurrent += damage;
+                    car.GetComponent<VehicleHypeBehavior>().AddHype(hypeToAdd);
+                }
             }
         }
-
     }
 }
