cdedbbabf26b686ca711458eb0c5528433a36f45
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-11-11 01:23:53 -0600

    Basic ability for Lux should be done
    Also moved some folders and files around to make project hierarchy easier to read

---
+++ AlmostRace-Capstone/Assets/Scripts/Abilities/Lux_Abilities/Lux_BasicAbility.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Eddie B. The code that handles the Lux's basic ability.
+ * Very similar to Volt_BasicAbility 2, but refactored and modified to perform better and fit with camera aiming
+ */
+
+public class Lux_BasicAbility : HeatAbility
+{
+    public GameObject luxProjectile;
+    private Lux_ProjectileBehavior _luxProjectileScript;
+
+    [Header("Ability Values")]
+
+    private bool _isFiring = false;
+    private bool _canFire = true;
+
+    [Tooltip("How much Hype is given per projectile.")]
+    public float projectileHypeToGain;
+
+    [Tooltip("How much damage is done per projectile bolt.")]
+    public float projectileDamage;
+
+    [Tooltip("How quickly each projectile moves.")]
+    public float projectileSpeed;
+
+    [Tooltip("How quickly the projectile are fired, smaller number means more projectiles!")]
+    public float rateOfFire;
+    private float _cooldownTime = 0;
+
+    [Tooltip("How fast firing projectiles builds heat.")]
+    public float selfDamageRate;
+
+    public List<Transform> muzzles;
+    private int _currentMuzzle;
+    private int _muzzleIterator = 1;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentMuzzle = 1;
+        carHeatInfo = gameObject.GetComponent<CarHeatManager>();
+        _luxProjectileScript = luxProjectile.GetComponent<Lux_ProjectileBehavior>();
+        _luxProjectileScript.SetImmunePlayer(gameObject);
+        _luxProjectileScript.SetProjectileInfo(projectileDamage, projectileSpeed, projectileHypeToGain);
+    }
+
+    private IEnumerator AbilityRateOfFire()
+    {
+        float tempTime = rateOfFire;
+
+        while (tempTime > 0)
+        {
+            tempTime -= Time.deltaTime;
+            yield return null;
+        }
+        _canFire = true;
+    }
+
+
+    public void FireProjectile()
+    {
+        if (_canFire)
+        {
+            AudioManager.instance.Play("Blaster1");
+            foreach (Transform i in muzzles)
+            {
+                i.LookAt(GetComponent<SphereCarController>().aimPos.transform);
+            }
+
+            GameObject projectile = Instantiate(luxProjectile, muzzles[_currentMuzzle].position, muzzles[_currentMuzzle].rotation);          
+            _canFire = false;
+            StartCoroutine(AbilityRateOfFire());
+            if (_currentMuzzle == (muzzles.Count - 1) || _currentMuzzle == 0)
+            {
+                _muzzleIterator *= -1;
+            }
+            _currentMuzzle += _muzzleIterator;
+        }
+    }
+
+    public override void ActivateAbility()
+    {
+        if (!_isFiring)
+        {
+            _isFiring = true;
+            InvokeRepeating("AddHeat", 0, selfDamageRate);
+            InvokeRepeating("FireProjectile", 0, rateOfFire);
+        }
+    }
+
+    public override void DeactivateAbility()
+    {
+        if (_isFiring)
+        {
+            _isFiring = false;
+            CancelInvoke("AddHeat");
+            CancelInvoke("FireProjectile");
+        }
+    }
+
+    protected override void AddHeat()
+    {
+        carHeatInfo.AddHeat(selfHeatDamage);
+    }
+
+
+}
---
+++ AlmostRace-Capstone/Assets/Scripts/Abilities/Lux_Abilities/Lux_ProjectileBehavior.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ Eddie B. This code is basically a copy past of Volt_LaserBolt (which will probably get deleted soon).
+ I made this script to allow us to edit behavior while keeping a temporary backup.
+ This script will no doubt become the final version eventually.
+     */
+
+public class Lux_ProjectileBehavior : MonoBehaviour
+{
+
+    public GameObject sparkEffect;
+    public MeshRenderer meshRenderer;
+    public Light pointLight;
+    private GameObject _immunePlayer;
+    private Vector3 _immunePlayerVelocity;
+    private float _speedActual;
+    private VehicleHypeBehavior _immunePlayerScript;
+    private Rigidbody _rigidBody;
+
+    private float _projectileDamage;
+    private float _projectileSpeed;
+    private float _projectileHype;
+    private bool _isAlive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody = gameObject.GetComponent<Rigidbody>();
+        _immunePlayerVelocity = _immunePlayer.gameObject.GetComponent<SphereCarController>().sphere.velocity;
+
+        _rigidBody.velocity = transform.TransformDirection(Vector3.forward * _projectileSpeed) + _immunePlayerVelocity;
+        _speedActual = _rigidBody.velocity.magnitude;
+
+        Destroy(gameObject, 7.0f);
+    }
+
+    public void SetImmunePlayer(GameObject immunePlayer)
+    {
+        _immunePlayer = immunePlayer;
+        _immunePlayerScript = _immunePlayer.GetComponent<VehicleHypeBehavior>();
+    }
+
+    public void SetProjectileInfo(float projectileDamage, float projectileSpeed, float projectileHypeToGain)
+    {
+        _projectileDamage = projectileDamage;
+        _projectileSpeed = projectileSpeed;
+        _projectileHype = projectileHypeToGain;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_isAlive)
+        {
+            if (other.gameObject != _immunePlayer && other.gameObject.GetComponent<CarHeatManager>() != null)
+            {//Checks if the object isn't the immunePlayer and if they are a car.
+                other.gameObject.GetComponent<CarHeatManager>().AddHeat(_projectileDamage);
+                _immunePlayerScript.AddHype(_projectileHype);
+                StartCoroutine(ExplosionEffect());
+            }
+            else if (other.gameObject != _immunePlayer && other.gameObject.GetComponent<Interactable>() != null)
+            {//Checks if the object isn't the immunePlayer and if they are an interactable object.
+                other.gameObject.GetComponent<Interactable>().interactingPlayer = _immunePlayer;
+                other.gameObject.GetComponent<Interactable>().DamageInteractable(_projectileDamage);
+                StartCoroutine(ExplosionEffect());
+            }
+            else if (other.gameObject != _immunePlayer)
+            {
+                StartCoroutine(ExplosionEffect());
+            }
+        }
+    }
+
+    private IEnumerator ExplosionEffect()
+    {
+        _isAlive = false;
+        _rigidBody.velocity = Vector3.zero;
+        _rigidBody.useGravity = false;
+        _rigidBody.isKinematic = true;
+        meshRenderer.enabled = false;
+        pointLight.enabled = false;
+        sparkEffect.SetActive(true);
+        yield return new WaitForSeconds(sparkEffect.GetComponent<ParticleSystem>().main.duration);
+        Destroy(gameObject);
+    }
+
+
+}
