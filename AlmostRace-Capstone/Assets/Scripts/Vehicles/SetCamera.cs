4738e840176030883b52322a77137a77d669aa06
Robyn Riley <rriley818@gmail.com>
2019-11-12 10:54:57 -0600

    Camera automatically sets based on player Number

---
+++ AlmostRace-Capstone/Assets/Scripts/Vehicles/SetCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class SetCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string layer = "Player" + GetComponent<VehicleInput>().getPlayerNum();
+        GetComponentInChildren<CinemachineVirtualCamera>().gameObject.layer = LayerMask.NameToLayer(layer);
+        GetComponentInChildren<Camera>().cullingMask += (1 << LayerMask.NameToLayer(layer));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
