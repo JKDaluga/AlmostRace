f21823d121f11dd066042c2136bac7b0e0779be1
JKDaluga <31351049+JKDaluga@users.noreply.github.com>
2019-11-25 20:49:03 -0600

    DataManager for storing player data between scenes

---
+++ AlmostRace-Capstone/Assets/Scripts/Managers/DataManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    private static DataManager instance;
+
+    public class PlayerInfo
+    {
+        public bool isActive = false;
+        public int carID = 0;
+        public int colorID = 0;
+    }
+
+    public PlayerInfo[] playerInfo;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+            playerInfo = new PlayerInfo[4];
+        }
+    }
+}
