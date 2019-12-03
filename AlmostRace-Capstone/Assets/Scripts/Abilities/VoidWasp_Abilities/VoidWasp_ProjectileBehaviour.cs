570afdc619e450fd2bc698016f56090d9f44b40a
Mike Romeo <mikeromeo@socialbrothers.nl>
2019-12-03 11:21:10 -0600

    shotgun blast

--- AlmostRace-Capstone/Assets/Scripts/Abilities/VoidWasp_Abilities/VoidWasp_ProjectileBehaviour.cs
+++ AlmostRace-Capstone/Assets/Scripts/Abilities/VoidWasp_Abilities/VoidWasp_ProjectileBehaviour.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class VoidWasp_ProjectileBehaviour : Projectile
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
+        if (_isAlive)
+        {
+            if (other.gameObject != _immunePlayer && other.gameObject.GetComponent<CarHeatManager>() != null)
+            {//Checks if the object isn't the immunePlayer and if they are a car.
+                other.gameObject.GetComponent<CarHeatManager>().AddHeat(_projectileDamage);
+                _immunePlayerScript.AddHype(_projectileHype, "Damage:");
+                StartCoroutine(ExplosionEffect());
+            }
+            else if (other.gameObject != _immunePlayer && other.gameObject.GetComponent<Interactable>() != null)
+            {//Checks if the object isn't the immunePlayer and if they are an interactable object.
+                if (other.gameObject.GetComponent<VoidWasp_ShieldBehaviour>() != null)
+                {//checks if about to hit a shield
+                    if (other.gameObject.GetComponent<VoidWasp_ShieldBehaviour>().GetShieldPlayer() == _immunePlayer)
+                    {
+                        return;
+                    }
+                }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+                other.gameObject.GetComponent<Interactable>().interactingPlayer = _immunePlayer;
+                other.gameObject.GetComponent<Interactable>().DamageInteractable(_projectileDamage);
+                StartCoroutine(ExplosionEffect());
+            }
+            else if (other.gameObject != _immunePlayer)
+            {
+                Debug.Log(gameObject.name + " has collided with: " + other.gameObject.name);
+                StartCoroutine(ExplosionEffect());
+            }
+        }
     }
 }
