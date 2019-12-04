8c14623e26ff8e9b496ef3c4574463ba9e6bac96
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-12-04 05:46:56 -0600

    playtest map like 40% done

--- AlmostRace-Capstone/Assets/Scripts/Hazards/Boulder/DestructableBoulderBehaviour.cs
+++ AlmostRace-Capstone/Assets/Scripts/Hazards/Boulder/DestructableBoulderBehaviour.cs
@@ -38,6 +38,8 @@ public class DestructableBoulderBehaviour : Interactable
         coll = this.GetComponent<Collider>();
         rend.enabled = true;
         coll.enabled = true;
+        boulderDestroyedHype *= transform.localScale.x;
+        interactableHealth *= (transform.localScale.x * 5);
     }
 
     private void OnCollisionEnter(Collision collision)
