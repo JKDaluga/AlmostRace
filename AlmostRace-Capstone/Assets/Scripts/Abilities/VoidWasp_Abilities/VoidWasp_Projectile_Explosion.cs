c7f5e3a44bed3666114b39d51243b2fe47b5a04a
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-12-10 00:32:30 -0600

    void wasp offensive "done"

--- AlmostRace-Capstone/Assets/Scripts/Abilities/VoidWasp_Abilities/VoidWasp_Projectile_Explosion.cs
+++ AlmostRace-Capstone/Assets/Scripts/Abilities/VoidWasp_Abilities/VoidWasp_Projectile_Explosion.cs
@@ -13,16 +13,19 @@ public class VoidWasp_Projectile_Explosion : MonoBehaviour
 
     private float _explosionRadius;
 
+    private GameObject _immunePlayer;
+
     Collider[] objectsHit;
 
 
 
-    public void GiveInfo(float explosionDamage, float explosionFuse, float explosionHypeToGain, float explosionRadius)
+    public void GiveInfo(float explosionDamage, float explosionFuse, float explosionHypeToGain, float explosionRadius, GameObject immunePlayer)
     {
         _explosionDamage = explosionDamage;
         _explosionFuse = explosionFuse;
         _explosionHypeToGain = explosionHypeToGain;
         _explosionRadius = explosionRadius;
+        _immunePlayer = immunePlayer;
        // Debug.Log("Explosion Fuse at Explosion is: " + _explosionFuse);
 
     }
@@ -35,27 +38,30 @@ public class VoidWasp_Projectile_Explosion : MonoBehaviour
 
     public void BlowFuse()
     {
-       objectsHit = Physics.OverlapSphere(gameObject.transform.localPosition, _explosionRadius);
-
+        Transform parent = gameObject.transform.parent;
+        gameObject.transform.SetParent(null);
+        objectsHit = Physics.OverlapSphere(gameObject.transform.localPosition, _explosionRadius);
+        gameObject.transform.SetParent(parent);
         foreach (Collider obj in objectsHit)
         {
-            Debug.Log("Object to be hit: " + obj.gameObject.name);
-            if (obj.gameObject.GetComponent<CarHeatManager>() != null)
-            {//if a car was hit
-                obj.gameObject.GetComponent<CarHeatManager>().AddHeat(_explosionDamage);
-                Debug.Log("Car was hit with explosion");
-
-            }
-            else if (obj.gameObject.GetComponent<Interactable>() != null)
+            if(obj.gameObject != _immunePlayer)
             {
-                obj.gameObject.GetComponent<Interactable>().DamageInteractable(_explosionDamage);
-                Debug.Log("Interactable was hit with explosion");
+                if (obj.gameObject.GetComponent<CarHeatManager>() != null)
+                {//if a car was hit
+                    obj.gameObject.GetComponent<CarHeatManager>().AddHeat(_explosionDamage);
 
+                    //Debug.Log("Car was hit with explosion");
+
+                }
+                else if (obj.gameObject.GetComponent<Interactable>() != null)
+                {
+                    obj.gameObject.GetComponent<Interactable>().DamageInteractable(_explosionDamage);
+                  
+                    // Debug.Log("Interactable was hit with explosion");
+
+                }
             }
-            else
-            {
-                Debug.Log("Ifs are definately fucking happening");
-            }
+
         }
        // DestroyExplosion(); 
     }
