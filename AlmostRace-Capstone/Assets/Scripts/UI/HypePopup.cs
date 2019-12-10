da15d05003c4f3c2830b4bafb75131294be973d4
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-12-09 23:35:53 -0600

    Lerped UI popups

--- AlmostRace-Capstone/Assets/Scripts/UI/HypePopup.cs
+++ AlmostRace-Capstone/Assets/Scripts/UI/HypePopup.cs
@@ -4,8 +4,47 @@ using UnityEngine;
 
 public class HypePopup : MonoBehaviour
 {
+
+    private Transform _startPos;
+    private Transform _endPos;
+    private float _duration;
+
     public void DestroyText()
     {
         Destroy(gameObject);
     }
+
+    public void GiveInfo(Transform startPos, Transform endPos, float duration)
+    {
+        _startPos = startPos;
+        _endPos = endPos;
+        _duration = duration;
+        StartCoroutine("StartLerp");
+    }
+
+    IEnumerator StartLerp()
+    {
+        float timePassed = 0;
+        float timeRatio = 0;
+        while(timePassed <= _duration)
+        {
+            timePassed += Time.deltaTime;
+            timeRatio = timePassed / _duration;
+            if (DataManager.instance.getNumActivePlayers() == 2)
+            {
+                this.gameObject.transform.localScale = (-2.22f * ((timeRatio - .25f) * (timeRatio - .25f)) + 1.25f) * (new Vector3(1, 2, 1));
+            }
+            else
+            {
+                this.gameObject.transform.localScale = (-2.22f * ((timeRatio - .25f) * (timeRatio - .25f)) + 1.25f) * (new Vector3(1, 1, 1));
+
+            }
+            gameObject.transform.position = Vector3.Lerp(_startPos.position, _endPos.position, timeRatio);
+            
+            yield return null;
+
+        }
+        Destroy(gameObject);
+    }
+
 }
