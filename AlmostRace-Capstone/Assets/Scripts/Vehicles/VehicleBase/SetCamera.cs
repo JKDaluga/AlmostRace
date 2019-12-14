67e038659331350e2308646a2ce3de1c938d495d
Jake Velicer <007velicer@gmail.com>
2019-12-14 16:54:07 -0600

    Fixed camera glitch

--- AlmostRace-Capstone/Assets/Scripts/Vehicles/VehicleBase/SetCamera.cs
+++ AlmostRace-Capstone/Assets/Scripts/Vehicles/VehicleBase/SetCamera.cs
@@ -11,6 +11,29 @@ public class SetCamera : MonoBehaviour
         string layer = "Player" + GetComponent<VehicleInput>().getPlayerNum();
         transform.parent.GetComponentInChildren<CinemachineVirtualCamera>().gameObject.layer = LayerMask.NameToLayer(layer);
         transform.parent.GetComponentInChildren<Camera>().cullingMask += (1 << LayerMask.NameToLayer(layer));
+
+        switch (GetComponent<VehicleInput>().playerNumber)
+        {
+            case 1:
+                transform.parent.GetComponentInChildren<CinemachineImpulseListener>().m_ChannelMask = 1;
+                GetComponent<CinemachineImpulseSource>().m_ImpulseDefinition.m_ImpulseChannel = 1;
+                break;
+            case 2:
+                transform.parent.GetComponentInChildren<CinemachineImpulseListener>().m_ChannelMask = 2;
+                GetComponent<CinemachineImpulseSource>().m_ImpulseDefinition.m_ImpulseChannel = 2;
+                break;
+            case 3:
+                transform.parent.GetComponentInChildren<CinemachineImpulseListener>().m_ChannelMask = 4;
+                GetComponent<CinemachineImpulseSource>().m_ImpulseDefinition.m_ImpulseChannel = 4;
+                break;
+            case 4:
+                transform.parent.GetComponentInChildren<CinemachineImpulseListener>().m_ChannelMask = 8;
+                GetComponent<CinemachineImpulseSource>().m_ImpulseDefinition.m_ImpulseChannel = 8;
+                break;
+        }
+
+
+        
     }
 
     // Update is called once per frame
