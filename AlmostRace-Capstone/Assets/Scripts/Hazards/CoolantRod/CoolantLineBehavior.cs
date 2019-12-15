f8a09208cb9999e255d4732ea312010ffe8c05de
jbove94 <jbove@mail.bradley.edu>
2019-12-15 14:36:23 -0600

    removed a debug message

--- AlmostRace-Capstone/Assets/Scripts/Hazards/CoolantRod/CoolantLineBehavior.cs
+++ AlmostRace-Capstone/Assets/Scripts/Hazards/CoolantRod/CoolantLineBehavior.cs
@@ -67,7 +67,7 @@ public class CoolantLineBehavior : MonoBehaviour
     {
         if(other.gameObject.GetComponent<CarHeatManager>() != null)
         {//Checks if a car passes through. 
-            Debug.Log("Car Detected by fire!");
+            //Debug.Log("Car Detected by fire!");
             _carsDamaged.Add(other.gameObject.GetComponent<CarHeatManager>()); //Adds car to damage.
             if(_carsDamaged.Count == 1)
             {//If this is the first car added to the list, assume that cars aren't being damaged, start the invoke repeating.
