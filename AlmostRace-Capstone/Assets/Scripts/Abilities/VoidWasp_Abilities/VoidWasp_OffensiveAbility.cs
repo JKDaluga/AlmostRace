13f65d35d5b5d3c8e92cb29def50325cdd126587
Robyn Riley <rriley818@gmail.com>
2019-12-15 18:49:57 -0600

    Predictive Aiming added to vehicle weapons

--- AlmostRace-Capstone/Assets/Scripts/Abilities/VoidWasp_Abilities/VoidWasp_OffensiveAbility.cs
+++ AlmostRace-Capstone/Assets/Scripts/Abilities/VoidWasp_Abilities/VoidWasp_OffensiveAbility.cs
@@ -97,9 +97,32 @@ public class VoidWasp_OffensiveAbility : HeatAbility
     {
         while (true)
         {
-            waspAimDirection.LookAt(_aimPosActual);
+            if (GetComponent<AimAssistant>().target != null && GetComponent<AimAssistant>().target.GetComponent<SphereCarController>() != null)
+            {
+                Vector3 assistedPos = predictedPosition(_aimPosActual.position, transform.position, GetComponent<AimAssistant>().target.GetComponent<Rigidbody>().velocity, projectileSpeed);
+                waspAimDirection.LookAt(assistedPos);
+            }
+            else
+            {
+                waspAimDirection.LookAt(_aimPosActual);
+            }
             yield return null;
         }
+    }
+
+    private Vector3 predictedPosition(Vector3 targetPosition, Vector3 shooterPosition, Vector3 targetVelocity, float projectileSpeed)
+    {
+        Vector3 displacement = targetPosition - shooterPosition;
+        float targetMoveAngle = Vector3.Angle(-displacement, targetVelocity) * Mathf.Deg2Rad;
+        //if the target is stopping or if it is impossible for the projectile to catch up with the target (Sine Formula)
+        if (targetVelocity.magnitude == 0 || targetVelocity.magnitude > projectileSpeed && Mathf.Sin(targetMoveAngle) / projectileSpeed > Mathf.Cos(targetMoveAngle) / targetVelocity.magnitude)
+        {
+            //  Debug.Log("Position prediction is not feasible.");
+            return targetPosition;
+        }
+        //also Sine Formula
+        float shootAngle = Mathf.Asin(Mathf.Sin(targetMoveAngle) * targetVelocity.magnitude / projectileSpeed);
+        return targetPosition + targetVelocity * displacement.magnitude / Mathf.Sin(Mathf.PI - targetMoveAngle - shootAngle) * Mathf.Sin(shootAngle) / targetVelocity.magnitude;
     }
 
     private IEnumerator AbilityRateOfFire()
