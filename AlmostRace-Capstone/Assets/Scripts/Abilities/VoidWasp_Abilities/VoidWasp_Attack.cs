9e1cd034135bb4b188bf9571ca8430ca4f09dca0
JakeVelicer <007velicer@gmail.com>
2020-02-05 03:33:51 -0600

    Started void wasp abilities

---
+++ AlmostRace-Capstone/Assets/Scripts/Abilities/VoidWasp_Abilities/VoidWasp_Attack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidWasp_Attack : Ability
+{
+    [Header("Ability Values")]
+    [Space(30)]
+    public GameObject voidwaspProjectile;
+    private bool _canFire = true;
+    private float _cooldownTime = 0;
+    [Tooltip("How quickly each projectile moves.")] public float projectileSpeed;
+    [Tooltip("How many projectile it shoots in total")] public int projectileCount = 8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void ActivateAbility()
+    {
+        if (_canFire)
+        {
+            
+        }
+    }
+
+    public override void DeactivateAbility()
+    {
+
+    }
+}
---
+++ AlmostRace-Capstone/Assets/Scripts/Abilities/VoidWasp_Abilities/VoidWasp_AttackTargetDetector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidWasp_AttackTargetDetector : MonoBehaviour
+{
+    private List<GameObject> _objectsInRange = new List<GameObject>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent("CarHealthBehavior") || other.gameObject.GetComponent("Interactable"))
+        {
+            _objectsInRange.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent("CarHealthBehavior") || other.gameObject.GetComponent("Interactable"))
+        {
+            for(int i = _objectsInRange.Count - 1; i >= 0; i--)
+            {
+                if(_objectsInRange[i] == other.gameObject)
+                {
+                    _objectsInRange.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
