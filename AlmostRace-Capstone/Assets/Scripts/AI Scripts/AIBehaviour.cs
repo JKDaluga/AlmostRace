97a6cb4619c8c7f3e94a4c83d10c09050ceb2efa
Robyn Riley <rriley818@gmail.com>
2020-02-11 11:17:31 -0600

    Delicious AI functionality. Obstacle Avoiding

--- AlmostRace-Capstone/Assets/Scripts/AI Scripts/AIBehaviour.cs
+++ AlmostRace-Capstone/Assets/Scripts/AI Scripts/AIBehaviour.cs
@@ -114,14 +114,14 @@ public class AIBehaviour : MonoBehaviour
         angleBetween = Mathf.Acos(Vector3.Dot((closestVertex-transform.position).normalized, thisCar.transform.right)) * 180 / Mathf.PI;
 
 
-        print("Current Turn Angle = " + angleBetween);
-        print("Current Turn number = " + inputTurn);
+        //print("Current Turn Angle = " + angleBetween);
+        //print("Current Turn number = " + inputTurn);
 
-        if (angleBetween < 87.5)
+        if (angleBetween < 84)
         {
             inputTurn =  Mathf.Pow((-(angleBetween - 90) / 90), (1/2));
         }
-        else if (angleBetween > 97.5)
+        else if (angleBetween > 100)
         {
             inputTurn =  -Mathf.Pow(((angleBetween - 90) / 90), (1/2));
         }
--- AlmostRace-Capstone/Assets/Scripts/AI Scripts/AIObstacleAvoidance.cs
+++ AlmostRace-Capstone/Assets/Scripts/AI Scripts/AIObstacleAvoidance.cs
@@ -9,30 +9,127 @@ public class AIObstacleAvoidance : MonoBehaviour
     RaycastHit left, right;
     public AIBehaviour handler;
 
-    public bool seeing;
+    public bool turnL, turnR;
+
+    float rayAngle, forwardAngle;
 
     public float turnAmount;
+
+    Vector3 vel;
+
+    public float turnOverride;
+    public float oneWallOvrd;
+
+    private void FixedUpdate()
+    {
+        vel = GetComponentInParent<Rigidbody>().velocity;
+        vel = new Vector3(vel.x, 0, vel.z);
+        transform.parent.rotation = Quaternion.LookRotation(vel, transform.parent.up);
+
+        if (turnL)
+        {
+            handler.inputTurn = -turnAmount;
+        }
+        if (turnR)
+        {
+            handler.inputTurn = turnAmount;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         Physics.Raycast(transform.parent.position, transform.right, out right, wall);
         Physics.Raycast(transform.parent.position, -transform.right, out left, wall);
 
-        seeing = true;
-        if(left.distance > right.distance)
+        //The Car only finds a wall on the left side
+        if ((left.collider != null && right.collider == null))
         {
-            handler.inputTurn = turnAmount;
+            //If the car is too far away, turn left
+            if (left.distance > oneWallOvrd)
+            {
+                turnL = true;
+                turnR = false;
+            }
+            //Otherwise, turn right
+            else
+            {
+                turnL = false;
+                turnR = true;
+            }
+        }
+        else if ((left.collider == null && right.collider != null))
+        {
+            //Code block opposite to left wall only
+            if (right.distance > oneWallOvrd)
+            {
+                turnL = false;
+                turnR = true;
+            }
+            else
+            {
+                turnL = true;
+                turnR = false;
+            }
+        }
+        //Checks if the left wall is closer than the right
+        else if (left.distance > right.distance)
+        {
+            //Gets the angle between the left wall normal and the drawn ray
+            rayAngle = Vector3.Angle(left.normal, -transform.right);
+            
+            //Checks if the car is moving diagonal to the wall
+            if (rayAngle > turnOverride)
+            {
+                //Gets the angle between the car forward and the wall normal
+                forwardAngle = Vector3.Angle(left.normal, transform.forward);
+                
+                //If the car is heading toward the wall, turn away, else turn toward it
+                if (forwardAngle > 90)
+                {
+                    turnL = false;
+                    turnR = true;
+                }
+                else
+                {
+                    turnL = true;
+                    turnR = false;
+                }
+            }
+            else
+            {
+                turnL = true;
+                turnR = false;
+            }
         }
         else
         {
-            handler.inputTurn = -turnAmount;
+            rayAngle = Vector3.Angle(right.normal, transform.right);
+            if (rayAngle > turnOverride)
+            {
+                forwardAngle = Vector3.Angle(right.normal, transform.forward);
+                if (forwardAngle > 90)
+                {
+                    turnL = true;
+                    turnR = false;
+                }
+                else
+                {
+                    turnL = false;
+                    turnR = true;
+                }
+            }
+            else
+            {
+                turnL = false;
+                turnR = true;
+            }
         }
 
     }
 
     private void OnTriggerExit(Collider other)
     {
-        handler.inputTurn = 0;
-        seeing = false;
+        turnL = false;
+        turnR = false;
     }
 }
