07b0321c99d05b00be9f973b00523fa79b8f7f3f
Gblevins20 <55031493+Gblevins20@users.noreply.github.com>
2020-02-13 11:34:50 -0600

    Award tracker update

--- AlmostRace-Capstone/Assets/Scripts/Vehicles/VehicleBase/VehicleAwardsTracker.cs
+++ AlmostRace-Capstone/Assets/Scripts/Vehicles/VehicleBase/VehicleAwardsTracker.cs
@@ -7,6 +7,7 @@ public class VehicleAwardsTracker : MonoBehaviour
     DataManager players;
     RaycastCar carInputs;
     VehicleAbilityBehavior abilityInputs;
+    VehicleInput playerNum;
 
     public float offensiveAwardTimer = 0f;
     public float defenseAwardTimer = 0f;
@@ -18,61 +19,41 @@ public class VehicleAwardsTracker : MonoBehaviour
         players = FindObjectOfType<DataManager>();
         carInputs = FindObjectOfType<RaycastCar>();
         abilityInputs = FindObjectOfType<VehicleAbilityBehavior>();
+        playerNum = FindObjectOfType<VehicleInput>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (abilityInputs.offensiveAbilityDark == true && offensiveAwardTimer <= 0f)
-        {
-            offenseUpdate();
-        }
-
-        if (abilityInputs.defensiveAbilityDark == true && defenseAwardTimer <= 0f)
-        {
-            defenseUpdate();
-        }
-
-        if (abilityInputs.boostAbilityDark == true && boostAwardTimer <= 0f)
-        {
-            boostUpdate();
-        }
-
         if (carInputs.drift)
         {
             driftUpdate();
         }
 
-
-        offensiveAwardTimer -= Time.deltaTime;
     }
 
-    private void offenseUpdate()
+    public void awardUpdate(char abilityType)
     {
-        players.playerInfo[0].offensiveAbilityUsed += 1;
-        print("Offensive ability used: " + players.playerInfo[0].offensiveAbilityUsed);
-        offensiveAwardTimer = abilityInputs.offensiveAbilityRecharge;
-    }
-
-    private void defenseUpdate()
-    {
-        players.playerInfo[0].defenseAbilityUsed += 1;
-        print("Defensive ability used: " + players.playerInfo[0].defenseAbilityUsed);
-        defenseAwardTimer = abilityInputs.defensiveAbilityRecharge;
-    }
-
-    private void boostUpdate()
-    {
-        players.playerInfo[0].boostAbilityUsed += 1;
-        print("Boost ability used: " + players.playerInfo[0].boostAbilityUsed);
-        boostAwardTimer = abilityInputs.boostAbilityRecharge;
+        if (abilityType == 'o')
+        {
+            players.playerInfo[playerNum.playerNumber].offensiveAbilityUsed += 1;
+        }
+        else if(abilityType=='d')
+        {
+            players.playerInfo[playerNum.playerNumber].defenseAbilityUsed += 1;
+        }
+        else
+        {
+            players.playerInfo[playerNum.playerNumber].boostAbilityUsed += 1;
+        }
     }
 
     private void driftUpdate()
     {
 
-        players.playerInfo[0].driftTimer += .1f;
+        players.playerInfo[playerNum.playerNumber].driftTimer += .1f;
         
     }
 }
