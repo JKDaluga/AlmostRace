d94602c2fd6c68a8341104d00c502923d8d49b92
Gblevins20 <55031493+Gblevins20@users.noreply.github.com>
2020-02-12 20:35:55 -0600

    Ability Tracker in awards

--- AlmostRace-Capstone/Assets/Scripts/Vehicles/VehicleBase/VehicleAwardsTracker.cs
+++ AlmostRace-Capstone/Assets/Scripts/Vehicles/VehicleBase/VehicleAwardsTracker.cs
@@ -6,25 +6,36 @@ public class VehicleAwardsTracker : MonoBehaviour
 {
     DataManager players;
     RaycastCar carInputs;
+    VehicleAbilityBehavior abilityInputs;
 
+    public float offensiveAwardTimer = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
         players = FindObjectOfType<DataManager>();
         carInputs = FindObjectOfType<RaycastCar>();
+        abilityInputs = FindObjectOfType<VehicleAbilityBehavior>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        if (abilityInputs.offensiveAbilityDark == true && offensiveAwardTimer <= 0f)
+        {
+            players.playerInfo[0].offensiveAbilityUsed += 1;
+            print("Offensive ability used: " + players.playerInfo[0].offensiveAbilityUsed);
+            offensiveAwardTimer = abilityInputs.offensiveAbilityRecharge;
+        }
+
         if (carInputs.drift)
         {
             driftUpdate();
-            print("Drift award amount: " + players.playerInfo[0].driftTimer);
         }
 
+
+        offensiveAwardTimer -= Time.deltaTime;
     }
 
     private void driftUpdate()
