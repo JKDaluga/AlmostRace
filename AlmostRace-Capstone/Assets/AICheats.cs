43114892a430e21fee8583a65c3222c2ff86b605
Robyn Riley <rriley818@gmail.com>
2020-02-25 11:17:22 -0600

    Baseline for AI Cheating is in

--- AlmostRace-Capstone/Assets/AICheats.cs
+++ AlmostRace-Capstone/Assets/AICheats.cs
@@ -1,18 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class AICheats : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<GameObject> players = new List<GameObject>();
+    RaycastCar rearPlayer;
+
+    public HypeGateBehavior arena;
+
+    [Tooltip("Number of Vertices away the AI can be before being warped")]
+    public float killDist = 40;
+
+    private void Start()
     {
-        
+        arena = FindObjectOfType<HypeGateBehavior>();
+        RaycastCar[] temp = FindObjectsOfType<RaycastCar>();
+
+        foreach(RaycastCar i in temp)
+        {
+            if (i.GetComponent<VehicleInput>())
+            {
+                players.Add(i.gameObject);
+            }
+        }
+        rearPlayer = players[0].GetComponent<RaycastCar>();
+
+        InvokeRepeating("distanceKill", 0, 2);
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    private void arenaWarp()
     {
-        
+        if(arena.carsInRange.Intersect(players) == players && !arena.carsInRange.Contains(gameObject))
+        {
+            GetComponent<CarHealthBehavior>().Kill();
+            GetComponent<AIBehaviour>().SwapSpline();
+        }
     }
+
+    private void distanceKill()
+    {
+        foreach(GameObject i in players)
+        {
+            if(i.GetComponent<RaycastCar>().closestIndex < rearPlayer.closestIndex)
+            {
+                rearPlayer = i.GetComponent<RaycastCar>();
+            }
+        }
+
+        if(rearPlayer.closestIndex - GetComponent<AIBehaviour>().closestIndex > 40)
+        {
+            GetComponent<CarHealthBehavior>().Kill();
+        }
+    }
+
 }
