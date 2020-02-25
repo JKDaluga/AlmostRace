7fbd5a9339cc083461293a5d90677461207acadb
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2020-02-25 09:20:54 -0600

    Windmill prefab started

---
+++ AlmostRace-Capstone/Assets/Scripts/Hazards/Windmill/WindmillBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindmillBehaviour : Interactable
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void DamageInteractable(float damageNumber)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void DestroyInteractable()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ResetInteractable()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void TriggerInteractable()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+}
