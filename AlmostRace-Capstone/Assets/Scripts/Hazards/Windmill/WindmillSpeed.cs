d7ff6d96f2ef5bbb340e2de1eac1bcd8c8a0e917
Gblevins20 <55031493+Gblevins20@users.noreply.github.com>
2020-02-26 21:05:20 -0600

    Windmill

---
+++ AlmostRace-Capstone/Assets/Scripts/Hazards/Windmill/WindmillSpeed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindmillSpeed : MonoBehaviour
+{
+
+    private float _laserSpeed;
+
+
+    void FixedUpdate()
+    {
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, Mathf.Lerp(transform.localEulerAngles.z, transform.localEulerAngles.z + _laserSpeed, 1));
+    }
+
+    public void UpdateBaseSpeed(float baseSpeed)
+    {
+        _laserSpeed = baseSpeed;
+    }
+}
