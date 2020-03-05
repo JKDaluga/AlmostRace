db4511710dc748672c8f710f4c1641e4f642df64
Gblevins20 <55031493+Gblevins20@users.noreply.github.com>
2020-03-05 11:38:55 -0600

    Pain Train Boosto w/o damage

--- AlmostRace-Capstone/Assets/Scripts/Abilities/PainTrain_Abilities/PainTrain_Boost.cs
+++ AlmostRace-Capstone/Assets/Scripts/Abilities/PainTrain_Abilities/PainTrain_Boost.cs
@@ -2,17 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PainTrain_Boost : MonoBehaviour
+public class PainTrain_Boost : CooldownHeatAbility
 {
+
+    private RaycastCar carInfo;
+    private bool isBoosting = false;
+
+    [Range(0, 1)]
+    public float boostSpeedPercentage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        carInfo = gameObject.GetComponent<RaycastCar>();
+        carHeatInfo = gameObject.GetComponent<CarHealthBehavior>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void ActivateAbility()
     {
-        
+        if (!isBoosting)
+        {
+            isBoosting = true;
+            carInfo.setBoostSpeed(boostSpeedPercentage);
+
+            AddHeat();
+        }
+    }
+
+    public override void DeactivateAbility()
+    {
+        carInfo.setBoostSpeed(0);
+
+        isBoosting = false;
+    }
+
+    protected override void AddHeat()
+    {
+        carHeatInfo.DamageCarTrue(selfHeatDamage);
     }
 }
