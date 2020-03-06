ac57c8fbfc07dc24ad72e685f108cdcc693999ed
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2020-03-06 15:24:11 -0600

    Pain Train Boost refined

--- AlmostRace-Capstone/Assets/Scripts/Abilities/PainTrain_Abilities/PainTrain_Boost.cs
+++ AlmostRace-Capstone/Assets/Scripts/Abilities/PainTrain_Abilities/PainTrain_Boost.cs
@@ -7,14 +7,10 @@ public class PainTrain_Boost : CooldownHeatAbility
 
     private RaycastCar carInfo;
     private bool isBoosting = false;
-    private CarHealthBehavior _immunePlayer;
     public GameObject boostField;
-    public float _boostDamageRate;
-    public float _boostDamage;
-
-    private Interactable hitInteractable;
-
-    private List<CarHealthBehavior> _damagedCars;
+    private PainTrain_BoostShock _boostFieldScript;
+    public float boostDamageRate;
+    public float boostDamage;
 
     [Range(0, 1)]
     public float boostSpeedPercentage;
@@ -22,10 +18,11 @@ public class PainTrain_Boost : CooldownHeatAbility
     // Start is called before the first frame update
     void Start()
     {
-        _damagedCars = new List<CarHealthBehavior>();
+        _boostFieldScript = boostField.GetComponent<PainTrain_BoostShock>();
+
+        _boostFieldScript.GiveInfo(gameObject, boostDamage, boostDamageRate);
+
         carInfo = gameObject.GetComponent<RaycastCar>();
-        carHeatInfo = gameObject.GetComponent<CarHealthBehavior>();
-        _immunePlayer = gameObject.GetComponent<CarHealthBehavior>();
     }
 
     public override void ActivateAbility()
@@ -39,65 +36,18 @@ public class PainTrain_Boost : CooldownHeatAbility
         }
     }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.GetComponent<CarHealthBehavior>()!=null && other.GetComponent<RaycastCar>() != carInfo)
-        {
-            _damagedCars.Add(other.GetComponent<CarHealthBehavior>());
-
-            if (_damagedCars.Count == 1)
-            {
-                InvokeRepeating("DamageCars", 0, _boostDamageRate);
-            }
-        }
-        else if (other.gameObject.GetComponent<Interactable>() != null)
-        {
-            hitInteractable = other.gameObject.GetComponent<Interactable>();
-            hitInteractable.DamageInteractable(hitInteractable.interactableHealth);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.GetComponent<CarHealthBehavior>() != null && _damagedCars.Contains(other.gameObject.GetComponent<CarHealthBehavior>()))
-        {
-            _damagedCars.Remove(other.gameObject.GetComponent<CarHealthBehavior>());
-        }
-    }
-
-    void DamageCars()
-    {
-        if (_damagedCars.Count != 0)
-        {
-            foreach (CarHealthBehavior car in _damagedCars)
-            {
-                if (!car.isDead)
-                {
-                    car.DamageCar(_boostDamage);
-                    
-                        if (car.healthCurrent <= 0)
-                        {
-                            _damagedCars.Remove(car);
-                        }
-                }
-            }
-        }
-        else
-        {
-            CancelInvoke("DamageCars");
-        }
-    }
-
     public override void DeactivateAbility()
     {
         carInfo.setBoostSpeed(0);
-
+        boostField.GetComponent<PainTrain_BoostShock>().ClearCarList();
         boostField.SetActive(false);
 
         isBoosting = false;
+
     }
 
     protected override void AddHeat()
     {
+        //nah
     }
 }
---
+++ AlmostRace-Capstone/Assets/Scripts/Abilities/PainTrain_Abilities/PainTrain_BoostShock.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PainTrain_BoostShock : MonoBehaviour
+{
+
+
+    private Interactable hitInteractable;
+
+    private List<CarHealthBehavior> _damagedCars;
+
+    private GameObject _immunePlayer;
+
+    private float _shockDamage;
+
+    private float _shockRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _damagedCars = new List<CarHealthBehavior>();
+    }
+
+    public void GiveInfo(GameObject immunePlayer, float shockDamage, float shockRate)
+    {
+        _immunePlayer = immunePlayer;
+        _shockDamage = shockDamage;
+        _shockRate = shockRate;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Vehicle") && other.gameObject != _immunePlayer)
+        {
+            _damagedCars.Add(other.GetComponent<CarHealthBehavior>());
+
+            if (_damagedCars.Count == 1)
+            {
+                InvokeRepeating("DamageCars", 0, _shockRate);
+            }
+        }
+        else if (other.gameObject.GetComponent<Interactable>() != null)
+        {
+            hitInteractable = other.gameObject.GetComponent<Interactable>();
+            hitInteractable.DamageInteractable(hitInteractable.interactableHealth);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<CarHealthBehavior>() != null && _damagedCars.Contains(other.gameObject.GetComponent<CarHealthBehavior>()))
+        {
+            _damagedCars.Remove(other.gameObject.GetComponent<CarHealthBehavior>());
+        }
+    }
+
+    public void ClearCarList()
+    {
+        _damagedCars.Clear();
+        CancelInvoke("DamageCars");
+    }
+
+    void DamageCars()
+    {
+        if (_damagedCars.Count != 0)
+        {
+            foreach (CarHealthBehavior car in _damagedCars)
+            {
+                if (!car.isDead)
+                {
+                    car.DamageCar(_shockDamage);
+                    if (car.healthCurrent <= 0)
+                    {
+                        _damagedCars.Remove(car);
+                    }
+                }
+            }
+        }
+        else
+        {
+            CancelInvoke("DamageCars");
+        }
+    }
+
+}
