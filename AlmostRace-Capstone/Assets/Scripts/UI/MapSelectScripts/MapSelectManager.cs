025cc27c50306b97485ebe689179284cd9101e82
JakeVelicer <007velicer@gmail.com>
2020-03-10 23:16:07 -0500

    Map Selection Almost Fully Implemented

---
+++ AlmostRace-Capstone/Assets/Scripts/UI/MapSelectScripts/MapSelectManager.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MapSelectManager : MonoBehaviour
+{
+    public string vehicleSelectScene;
+    public int mineMapScene = 3;
+    public int stellarMapScene = 4;
+    public GameObject[] playerLayouts;
+    public MapSelectViewport[] viewports;
+    public VehicleInput[] playerInputs;
+    public TextMeshProUGUI _mineMapVoteText;
+    public TextMeshProUGUI _stellarMapVoteText;
+    public GameObject allReadyImage;
+    private DataManager _data;
+    private static bool _readyToStart = false;
+    private static bool _isLoading = false;
+    private int mineMapVotes = 0;
+    private int stellarMapVotes = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _data = DataManager.instance;
+        _isLoading = false;
+        _data.resetData();
+
+        foreach(GameObject layout in playerLayouts)
+        {
+            layout.SetActive(false);
+        }
+
+        int numPlayers = _data.getNumActivePlayers();
+        switch (numPlayers)
+        {
+            case 0:
+                viewports = new MapSelectViewport[1];
+                break;
+            case 1:
+                viewports = new MapSelectViewport[2];
+                break;
+            case 2:
+                viewports = new MapSelectViewport[3];
+                break;
+            case 3:
+                viewports = new MapSelectViewport[4];
+                break;
+            default:
+                Debug.LogError("Incorrect amount of players recieved");
+                break;
+        }
+        viewports = playerLayouts[numPlayers].GetComponentsInChildren<MapSelectViewport>();
+        playerLayouts[numPlayers].SetActive(true);
+        allReadyImage.SetActive(false);
+        for (int i = 0; i < viewports.Length; i++)
+        {
+           CheckController(playerInputs[_data.playerInfo[i].controllerID]);
+        }
+    }
+
+    private void Update()
+    {
+        if(_readyToStart && !_isLoading)
+        {
+            _isLoading = true;
+            if (mineMapVotes > stellarMapVotes)
+            {
+                SceneManager.LoadSceneAsync(mineMapScene);
+            }
+            else if (stellarMapVotes > mineMapVotes)
+            {
+                SceneManager.LoadSceneAsync(stellarMapScene);
+            }
+            else
+            {
+                SceneManager.LoadSceneAsync(Random.Range(3,5));
+            }
+        }
+    }
+
+    private void CheckController(VehicleInput givenController)
+    {
+        bool inUse = false;
+        for (int i = 0; i < viewports.Length; i++)
+        {
+            if (viewports[i].GetOccupied() && viewports[i].GetInput() == givenController)
+            {
+                inUse = true;
+                break;
+            }
+        }
+        if (!inUse)
+        {
+            AssignPlayer(givenController);
+        }
+    }
+
+    private void AssignPlayer(VehicleInput givenController)
+    {
+        for (int i = 0; i < viewports.Length; i++)
+        {
+            if (!viewports[i].GetOccupied())
+            {
+                viewports[i].PlayerJoin(true, givenController);
+                break;
+            }
+        }
+    }
+
+    public void UpdateData(bool isReady, int givenMapSelection)
+    {
+        if (isReady)
+        {
+            if (givenMapSelection == 1)
+            {
+                mineMapVotes = mineMapVotes + 1;
+            }
+            else if (givenMapSelection == 2)
+            {
+                stellarMapVotes = stellarMapVotes + 1;
+            }
+        }
+        else if (!isReady)
+        {
+            if (givenMapSelection == 1)
+            {
+                mineMapVotes = mineMapVotes - 1;
+            }
+            else if (givenMapSelection == 2)
+            {
+                stellarMapVotes = stellarMapVotes - 1;
+            }            
+        }
+        else
+        {
+            Debug.LogError("Invalid player map selection given");
+        }
+        _mineMapVoteText.text = mineMapVotes.ToString();
+        _stellarMapVoteText.text = stellarMapVotes.ToString();
+        if (IsReady())
+        {
+            _readyToStart = true;
+            allReadyImage.SetActive(true);
+        }
+    }
+
+    private bool IsReady()
+    {
+        bool enoughPlayers = false;
+        for(int i = 0; i < viewports.Length; i++)
+        {
+            if (viewports[i].GetOccupied() == true)
+            {
+                enoughPlayers = true;
+                if (!viewports[i].GetSelected())
+                {
+                    return false;
+                }
+            }
+        }
+        if (enoughPlayers)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool GetReadyToStart()
+    {
+        return _readyToStart;
+    }
+
+    public void LoadVehicleSelect()
+    {
+        SceneManager.LoadSceneAsync(vehicleSelectScene);
+    }
+}
---
+++ AlmostRace-Capstone/Assets/Scripts/UI/MapSelectScripts/MapSelectViewport.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MapSelectViewport : MonoBehaviour
+{
+    public MapSelectManager mapSelectManager;
+
+    [Header("UI Values")]
+    [Space(20)]
+    public Image mineMapSelect;
+    public Image stellarMapSelect;
+    public Image coverBox;
+    public Image mineSnapshot;
+    public Image stellarSnapshot;
+    public Image[] mineSnapshotBehind;
+    public Image[] stellarSnapshotBehind;
+    public TextMeshProUGUI _statusText;
+
+    private int _mapSelected;
+    private VehicleInput _playerInput;
+    private bool _selected;
+    private bool _isSelecting;
+    private bool _occupied;
+
+    private void Start()
+    {
+        coverBox.enabled = false;
+        mineMapSelect.enabled = true;
+        stellarMapSelect.enabled = false;
+        mineSnapshot.enabled = true;
+        stellarSnapshot.enabled = false;
+        _mapSelected = 1;
+        foreach (Image snapshot in mineSnapshotBehind)
+        {
+            snapshot.enabled = false;
+        }
+        foreach (Image snapshot in stellarSnapshotBehind)
+        {
+            snapshot.enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (!mapSelectManager.GetReadyToStart())
+        {
+            if (!_selected)
+            {
+                MapScroll();
+                if (Input.GetButtonDown(_playerInput.selectButton))
+                {
+                    MapSelect(true);
+                }
+                else if (Input.GetButtonDown(_playerInput.backButton))
+                {
+                    PlayerJoin(false, null);
+                }
+            }
+            else
+            {
+                if (Input.GetButtonDown(_playerInput.backButton))
+                {
+                    MapSelect(false);
+                }
+            }
+        }
+    }
+
+    private void MapScroll()
+    {
+        if (Input.GetAxis(_playerInput.horizontal) < -0.3f)
+        {
+            if(!_isSelecting)
+            {
+                HighlightMineMap();
+            }
+        }
+        else if (Input.GetAxis(_playerInput.horizontal) > 0.3f)
+        {
+            if(!_isSelecting)
+            {
+                HighlightInterstellareMap();
+            }
+        }
+    }
+
+    private void HighlightMineMap()
+    {
+        _isSelecting = true;
+        stellarMapSelect.enabled = false;
+        mineMapSelect.enabled = true;
+        mineSnapshot.enabled = true;
+        stellarSnapshot.enabled = false;
+        _mapSelected = 1;
+        _isSelecting = false;
+    }
+
+    private void HighlightInterstellareMap()
+    {
+        _isSelecting = true;
+        mineMapSelect.enabled = false;
+        stellarMapSelect.enabled = true;   
+        stellarSnapshot.enabled = true;
+        mineSnapshot.enabled = false;
+        _mapSelected = 2;
+        _isSelecting = false;
+    }
+    
+    public void PlayerJoin(bool status, VehicleInput controllerNumber)
+    {
+        if (status == true)
+        {
+            _occupied = true;
+            _playerInput = controllerNumber;
+            _statusText.text = "PRESS A TO CONFIRM";
+            coverBox.enabled = false;
+            mineMapSelect.enabled = true;
+            stellarMapSelect.enabled = false;
+            mineSnapshot.enabled = true;
+            stellarSnapshot.enabled = false;
+            foreach (Image snapshot in mineSnapshotBehind)
+            {
+                snapshot.enabled = false;
+            }
+            foreach (Image snapshot in stellarSnapshotBehind)
+            {
+                snapshot.enabled = false;
+            }
+
+        }
+        else if (status == false)
+        {
+            mapSelectManager.LoadVehicleSelect();
+            _occupied = false;
+        }
+    }
+
+    private void MapSelect(bool status)
+    {
+        if (status == true)
+        {
+            _selected = true;
+            coverBox.enabled = true;
+            _statusText.text = "PRESS B TO UNCONFIRM";
+            if (_mapSelected == 1)
+            {
+                foreach (Image snapshot in mineSnapshotBehind)
+                {
+                    snapshot.enabled = true;
+                }
+                foreach (Image snapshot in stellarSnapshotBehind)
+                {
+                    snapshot.enabled = false;
+                }
+            }
+            else if (_mapSelected == 2)
+            {
+                foreach (Image snapshot in mineSnapshotBehind)
+                {
+                    snapshot.enabled = false;
+                }
+                foreach (Image snapshot in stellarSnapshotBehind)
+                {
+                    snapshot.enabled = true;
+                }
+            }
+            mapSelectManager.UpdateData(_selected, _mapSelected);
+        }
+        else
+        {
+            _selected = false;
+            coverBox.enabled = false;
+            _statusText.text = "PRESS A TO CONFIRM";
+            foreach (Image snapshot in mineSnapshotBehind)
+            {
+                snapshot.enabled = false;
+            }
+            foreach (Image snapshot in stellarSnapshotBehind)
+            {
+                snapshot.enabled = false;
+            }
+            mapSelectManager.UpdateData(_selected, _mapSelected);
+            
+        }
+    }
+
+    public bool GetOccupied()
+    {
+        return _occupied;
+    }
+
+    public bool GetSelected()
+    {
+        return _selected;
+    }
+
+    public VehicleInput GetInput()
+    {
+        return _playerInput;
+    }
+}
