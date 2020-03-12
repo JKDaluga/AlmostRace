35931744d8b6b0d31d74c7d4fb0d536d3e33b965
JakeVelicer <007velicer@gmail.com>
2020-03-12 07:21:56 -0500

    Shortcuts work in test scenes

---
+++ AlmostRace-Capstone/Assets/Scripts/StellarMapShortcut.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StellarMapShortcut : Shortcut
+{
+    public GameObject[] elementsToTurnOFF;
+
+    public override void ActivateAbility()
+    {
+        foreach (GameObject blocker in elementsToTurnOFF)
+        {
+            blocker.SetActive(false);
+        }
+    }
+
+}
