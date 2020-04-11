6639c4bb39ec9a0033ba662ce7e7986576ed52bc
Gblevins20 <55031493+Gblevins20@users.noreply.github.com>
2020-04-11 14:09:15 -0500

    Button Beep

---
+++ AlmostRace-Capstone/Assets/Scripts/UI/MenuScripts/ButtonBeep.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBeep : MonoBehaviour
+{
+
+    public void Beep()
+    {
+        AudioManager.instance.PlayWithoutSpatial("Lux Shooting");
+    }
+}
