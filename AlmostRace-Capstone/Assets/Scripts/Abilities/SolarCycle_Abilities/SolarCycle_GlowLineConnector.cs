e5321df36d85cf243c3de83897d7b64ede8485a2
JakeVelicer <007velicer@gmail.com>
2020-04-13 05:41:18 -0500

    Render Line For Solar Sycle in

---
+++ AlmostRace-Capstone/Assets/Scripts/Abilities/SolarCycle_Abilities/SolarCycle_GlowLineConnector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarCycle_GlowLineConnector : MonoBehaviour
+{
+    public LineRenderer lineRender;
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRender.SetPosition(0, transform.position);
+        lineRender.SetPosition(1, target.position);
+    }
+}
