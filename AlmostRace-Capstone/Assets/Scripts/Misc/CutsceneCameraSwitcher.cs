2c34082c1507527072bacd8b965cce9cc3934b6d
JakeVelicer <007velicer@gmail.com>
2020-04-19 18:15:31 -0500

    Cutscene Camera Switcher

---
+++ AlmostRace-Capstone/Assets/Scripts/Misc/CutsceneCameraSwitcher.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneCameraSwitcher : MonoBehaviour
+{
+    public Camera[] cutsceneCameras;
+    private int currentCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (currentCamera >= cutsceneCameras.Length - 1)
+            {
+                currentCamera = 0;
+            }
+            else
+            {
+                currentCamera++;
+            }
+            SwitchCamera();
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (currentCamera <= 0)
+            {
+                currentCamera = cutsceneCameras.Length - 1;
+            }
+            else
+            {
+                currentCamera--;
+            }
+            SwitchCamera();
+        }
+    }
+
+    private void SwitchCamera()
+    {
+        for(int i = 0; i < cutsceneCameras.Length; i++)
+        {
+            if (i == currentCamera)
+            {
+                cutsceneCameras[i].enabled = true;
+            }
+            if (cutsceneCameras[i].enabled == true && i != currentCamera)
+            {
+                cutsceneCameras[i].enabled = false;
+            }
+        }
+    }
+}
