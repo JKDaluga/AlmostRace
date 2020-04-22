a8f07731d1408e49c3dbcf54908f5266beedf004
JKDaluga <31351049+JKDaluga@users.noreply.github.com>
2020-04-22 14:37:57 -0500

    Alt-Tabing no longer loses the selected object

---
+++ AlmostRace-Capstone/Assets/Scripts/Managers/FocusManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FocusManager : MonoBehaviour
+{
+    EventSystem eventSystem;
+    GameObject selectedGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eventSystem = GetComponent<EventSystem>();
+    }
+    void OnApplicationFocus(bool hasFocus)
+    {
+        eventSystem = GetComponent<EventSystem>();
+        if(eventSystem != null)
+        {
+            selectedGameObject = eventSystem.currentSelectedGameObject;
+        }
+    }
+
+    void OnApplicationPause(bool pauseStatus)
+    {
+        eventSystem = GetComponent<EventSystem>();
+        if (eventSystem != null && selectedGameObject != null)
+        {
+            eventSystem.SetSelectedGameObject(selectedGameObject);
+        }
+    }
+}
