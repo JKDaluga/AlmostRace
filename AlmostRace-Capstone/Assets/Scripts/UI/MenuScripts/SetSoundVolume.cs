5af7014df0641727646e4b32a273ea90bbfa53f2
Gblevins20 <55031493+Gblevins20@users.noreply.github.com>
2020-04-22 20:01:01 -0500

    Settings Menu Updates
    Settings menu update

--- AlmostRace-Capstone/Assets/Scripts/UI/MenuScripts/SetSoundVolume.cs
+++ AlmostRace-Capstone/Assets/Scripts/UI/MenuScripts/SetSoundVolume.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class SetSoundVolume : MonoBehaviour
 {
     public SettingsMenuScript SoundVolume;
+    
 
     //Just a dummy function that is meant to allow a slider to transfer its value to the settings menu
     public void SetVolume(float SliderValue)
     {
         SoundVolume.SetSoundFXVoume(SliderValue);
         
-            AudioManager.instance.Play("Menu Selection", transform);
+        AudioManager.instance.PlayWithoutSpatial("Menu Selection");
+
         
     }
 }
--- AlmostRace-Capstone/Assets/Scripts/UI/MenuScripts/SettingsMenuScript.cs
+++ AlmostRace-Capstone/Assets/Scripts/UI/MenuScripts/SettingsMenuScript.cs
@@ -10,13 +10,21 @@ public class SettingsMenuScript : MonoBehaviour
     public AudioManager volumeControl;
 
     public Slider musicSlider;
+    public Image mSliderArrowLeft, mSliderArrowRight;
+
     public Slider soundFXSlider;
+    public Image sSliderArrowLeft, sSliderArrowRight;
+
+    private float mSliderPrevious, sSliderPrevious;
 
     // Start is called before the first frame update
     void Start()
     {
         musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
         soundFXSlider.value = PlayerPrefs.GetFloat("SoundFXVolume", 1f);
+
+        mSliderPrevious = musicSlider.value;
+        sSliderPrevious = soundFXSlider.value;
 
         volumeControl.updateSoundVolume(soundFXSlider.value);
     }
@@ -30,11 +38,37 @@ public class SettingsMenuScript : MonoBehaviour
     {
         VolumeMusic.volume = SliderValue;
         PlayerPrefs.SetFloat("MusicVolume", SliderValue);
+
+        if (musicSlider.value > mSliderPrevious)
+        {
+            mSliderArrowRight.color = new Color32(243, 238, 128, 255);
+            mSliderPrevious = musicSlider.value;
+            mSliderArrowLeft.color = new Color32(12, 193, 184, 255);
+        }
+        else if (musicSlider.value < mSliderPrevious)
+        {
+            mSliderArrowLeft.color = new Color32(243, 238, 128, 255);
+            mSliderPrevious = musicSlider.value;
+            mSliderArrowRight.color = new Color32(12, 193, 184,255);
+        }
     }
 
     public void SetSoundFXVoume(float SliderValue)
     {
         volumeControl.updateSoundVolume(SliderValue);
         PlayerPrefs.SetFloat("SoundFXVolume", SliderValue);
+
+        if (soundFXSlider.value > sSliderPrevious)
+        {
+            mSliderArrowRight.color = new Color32(243, 238, 128, 255);
+            mSliderPrevious = soundFXSlider.value;
+            mSliderArrowLeft.color = new Color32(12, 193, 184, 255);
+        }
+        else if (soundFXSlider.value < sSliderPrevious)
+        {
+            sSliderArrowLeft.color = new Color32(243, 238, 128, 255);
+            sSliderPrevious = soundFXSlider.value;
+            sSliderArrowRight.color = new Color32(12, 193, 184, 255);
+        }
     }
 }
