d7809a4156d491f8e83ad278655d16c0bb3ddff2
Gblevins20 <55031493+Gblevins20@users.noreply.github.com>
2020-04-26 14:11:10 -0500

    Main Menu Bug Fixes and Updates
    Fixed the bug where it couldn't find the sound file. Also made it so sounds are updated on start.

---
+++ AlmostRace-Capstone/Assets/Scripts/UI/MenuScripts/SoundStartUpScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundStartUpScript : MonoBehaviour
+{
+    public SettingsMenuScript soundChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //soundChange.StartUpdate();
+    }
+
+    
+}
