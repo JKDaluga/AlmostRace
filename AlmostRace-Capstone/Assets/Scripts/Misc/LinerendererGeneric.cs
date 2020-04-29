f16435ef6fc03c9bd9e56c517555729b22760ce5
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2020-04-28 20:15:43 -0500

    new windmills

---
+++ AlmostRace-Capstone/Assets/Scripts/Misc/LinerendererGeneric.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinerendererGeneric : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    public Transform start;
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, start.position);
+        lineRenderer.SetPosition(1, target.position);
+    }
+}
