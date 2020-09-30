462669e32ebf2863c8bba0fddf47b2b19f27cec2
JakeVelicer <007velicer@gmail.com>
2020-09-29 19:12:38 -0500

    Auto Deactivate

---
+++ AlmostRace-Capstone/Assets/Scripts/Misc/DeactivateObjectsOnStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateObjectsOnStart : MonoBehaviour
+{
+    public GameObject[] objectsToDeactivate;
+
+    void Start()
+    {
+        for (int i = 0; i < objectsToDeactivate.Length; i++)
+        {
+            objectsToDeactivate[i].SetActive(false);
+        }
+    }
+}
